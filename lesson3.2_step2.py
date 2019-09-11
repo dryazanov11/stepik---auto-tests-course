89b2d54da51ed1b8aef4d6769314ef5cd6beeb65
dryazanov1 <dryazanov@assist.ru>
2019-09-11 16:45:49 +0300

    добавил вторую задачу с ассертом

---
+++ lesson3.2_step2.py
@@ -0,0 +1,8 @@
+﻿def test_substring(full_string, substring):
+    x = substring
+    y = full_string
+    assert x in y, (f"expected '{x}' to be substring of '{y}'")
+	
+full_string = 'fulltext'
+substring = 'some_value'
+print (test_substring(full_string, substring))
