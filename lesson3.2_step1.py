fb7c385bf95066a32192d743bb53e4affae83cf9
dryazanov1 <dryazanov@assist.ru>
2019-09-11 14:27:41 +0300

    добавил задачу с ассертом

---
+++ lesson3.2_step1.py
@@ -0,0 +1,13 @@
+﻿from selenium import webdriver
+import time
+import math
+
+def test_input_text(expected_result, actual_result):
+    if expected_result != actual_result:
+        return f"expected {expected_result}, got {actual_result}"
+    
+
+
+expected_result = 8
+actual_result = 11
+print (test_input_text(expected_result, actual_result))
